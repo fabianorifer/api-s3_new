4a0adaf8e17c31ccd83de8120b488bca0024da43
Luis Fabiano Ivor Rivadeneira Fernandez <166071518+fabianorifer@users.noreply.github.com>
2024-11-11 11:12:35 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -3,26 +3,25 @@ import boto3
 from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
-    # Initialize the S3 client
     s3_client = boto3.client('s3')
-
-    # Retrieve bucket name and region from the event
     bucket_name = event.get('bucket_name', 'default-bucket-name')
-    region = event.get('region', 'us-east-1')  # Default to 'us-east-1' if not provided
+    region = event.get('region', 'us-east-1')
 
     try:
-        # Create the S3 bucket
-        response = s3_client.create_bucket(
-            Bucket=bucket_name,
-            CreateBucketConfiguration={
-                'LocationConstraint': region
-            }
-        )
+        # Crea el bucket sin LocationConstraint para us-east-1
+        if region == 'us-east-1':
+            response = s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            response = s3_client.create_bucket(
+                Bucket=bucket_name,
+                CreateBucketConfiguration={'LocationConstraint': region}
+            )
+
         return {
             'statusCode': 200,
             'body': json.dumps(f'Successfully created bucket: {bucket_name}')
         }
-        
+
     except ClientError as e:
         return {
             'statusCode': 501,
