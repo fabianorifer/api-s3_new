388ac8c95b4029dace7009425b47445f8e72cfea
Luis Fabiano Ivor Rivadeneira Fernandez <166071518+fabianorifer@users.noreply.github.com>
2024-11-11 11:04:56 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -2,6 +2,14 @@ import boto3
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
+    
+    # Verificar si 'bucket_name' está en event
+    if 'bucket_name' not in event:
+        return {
+            'statusCode': 400,
+            'body': 'Error: El campo bucket_name es obligatorio.'
+        }
+
     bucket_name = event['bucket_name']
     
     response = s3.create_bucket(
